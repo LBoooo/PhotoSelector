3cf651cfc01c8072da7b72c1512032d87aa67988
Hinacle <workerlibai@163.com>
2021-10-23 17:27:54 +0800

    视频只能选择一个

--- photoselector/src/main/java/com/kelin/photoselector/ui/AlbumFragment.kt
+++ photoselector/src/main/java/com/kelin/photoselector/ui/AlbumFragment.kt
@@ -330,30 +330,23 @@ internal class AlbumFragment : BasePhotoSelectorFragment() {
             itemView.rlKelinPhotoSelectorChecker.setOnClickListener {
                 listAdapter.getItem(layoutPosition).apply {
                     val selectedPictures = listAdapter.selectedPictures
-
-
                     if (isSelectImageOrVideo) {
                         if (selectedPictures.isNotEmpty()){
-                            var selectType = selectedPictures[0].isVideo
+                            val selectType = selectedPictures[0].isVideo
                             if (selectType){
                                 // 只能选择视频
                                 if (!isVideo){
                                     Toast.makeText(applicationContext, "只能选择图片或视频", Toast.LENGTH_SHORT).show()
-
                                     return@setOnClickListener
                                 }
                             }else{
                                 // 只能选择图片
                                 if (isVideo){
                                     Toast.makeText(applicationContext, "只能选择图片或视频", Toast.LENGTH_SHORT).show()
-
                                     return@setOnClickListener
                                 }
                             }
-
-
                         }
-
                     }
 
                     //如果被选中了的资源中没有当前的资源，那么就认为当前用户的目的是选中，否则就是取消选中。
@@ -365,7 +358,6 @@ internal class AlbumFragment : BasePhotoSelectorFragment() {
                             if (PhotoSelector.isAutoCompress && !isVideo) {
                                 compressAndRotateByDegree()
                             }
-
                             selectedPictures.add(this)
                         } else {
                             //取消选中时判断是否是取消的最后一个，如果不是的话那还要刷新其他的条目变更序号。
@@ -386,6 +378,10 @@ internal class AlbumFragment : BasePhotoSelectorFragment() {
                     }
                     if (isSingleSelector) {
                         onSelectDone()
+                    }
+                    if (isSelectImageOrVideo) {
+                        if (isVideo)
+                            onSelectDone()
                     }
                 }
             }
